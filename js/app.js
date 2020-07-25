5a12e687138a3cda49e9a44838e2f35f16a2976d
LuisAltamiranoAnicama <luis21_023@hotmail.com>
2020-07-25 11:36:45 -0500

    farmacia v2

--- js/app.js
+++ js/app.js
@@ -48,9 +48,59 @@ tabla.innerHTML = ``;
         <td>${doc.data().nombre}</td>
         <td>${doc.data().descripcion}</td>
         <td>${doc.data().precio}</td>
+        <td><button class="btn btn-danger" onclick="eliminar('${doc.id}')">Eliminar</button></td>
+        <td><button class="btn btn-warning" onclick="editar('${doc.id}', '${doc.data().nombre}', '${doc.data().descripcion}', '${doc.data().precio}')">Editar</button></td>
         </tr>
         `
     });
 });
+}
+
+function eliminar(id){
+    db.collection("medicamento").doc(id).delete().then(function() {
+        console.log("Document successfully deleted!");
+    }).catch(function(error) {
+        console.error("Error removing document: ", error);
+    });
+}
+
+function editar(id, nombre, descripcion, precio){
+
+    document.getElementById("nombre").value = nombre;
+    document.getElementById("descripcion").value = descripcion;
+    document.getElementById("precio").value = precio;
+    
+    var boton = document.getElementById("boton");
+    boton.innerHTML = 'Editar';
+
+    boton.onclick = function(){
+
+        var medicamento = db.collection("medicamento").doc(id);
+
+        var name = document.getElementById("nombre").value;
+        var description = document.getElementById("descripcion").value;
+        var price = document.getElementById("precio").value;
+
+        // Set the "capital" field of the city 'DC'
+        return medicamento.update({
+            nombre: name,
+            descripcion: description,
+            precio: price
+        
+        })
+        .then(function() {
+            console.log("Document successfully updated!");
+            boton.innerHTML = 'Guardar';
+            document.getElementById("nombre").value = "";
+            document.getElementById("descripcion").value = "";
+            document.getElementById("precio").value =0;
+
+        })
+        .catch(function(error) {
+            // The document probably doesn't exist.
+            console.error("Error updating document: ", error);
+        });
+
+    }
 }
 
