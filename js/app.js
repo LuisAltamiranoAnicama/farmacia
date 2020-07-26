4e3d1b795d771e76b3fdd2fc12e12ac3eef02fa4
Luis Eloy Altamirano Anicama <42721086+LuisAltamiranoAnicama@users.noreply.github.com>
2020-07-26 13:19:27 -0500

    Update app.js
    Version 3.0

--- js/app.js
+++ js/app.js
@@ -9,32 +9,73 @@ firebase.initializeApp({
   
   var db = firebase.firestore();
 
-listar();
+  listar();
 
 function guardar(){
     
     var name = document.getElementById("nombre").value;
     var description = document.getElementById("descripcion").value;
     var price = document.getElementById("precio").value;
+    if (name.trim()!= "" && price >0){
 
-db.collection("medicamento").add({
-    descripcion: description,
-    nombre: name,
-    precio: price
-    
-})
-.then(function(docRef) {
-    console.log("error");
-    console.log("Document written with ID: ", docRef.id);
-    document.getElementById("nombre").value = "";
-    document.getElementById("descripcion").value = "";
-    document.getElementById("precio").value =0;
-})
-.catch(function(error) {
-    console.error("Error adding document: ", error);
-});
+        db.collection("medicamento").add({
+            descripcion: description,
+            nombre: name,
+            precio: prices
+            
+        })
+        .then(function(docRef) {
+            console.log("error");
+            console.log("Document written with ID: ", docRef.id);
+            document.getElementById("nombre").value = "";
+            document.getElementById("descripcion").value = "";
+            document.getElementById("precio").value =0;
+        })
+        .catch(function(error) {
+            console.error("Error adding document: ", error);
+        });
+        
+        listar();
+    }
 
-listar();
+}
+
+function buscar(){
+
+    var nombre_buscar = document.getElementById("txtmedicamento").value;
+
+    if (nombre_buscar.trim() == ""){
+        listar();
+    }
+
+    else{
+
+        var tabla = document.getElementById("tabla");
+
+    db.collection("medicamento").where("nombre", "==", nombre_buscar)
+    .get()
+    .then(function(querySnapshot) {
+
+    tabla.innerHTML = ``;
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data().nombre}`);
+        tabla.innerHTML += `
+        <tr>
+        <td>${doc.data().nombre}</td>
+        <td>${doc.data().descripcion}</td>
+        <td>${doc.data().precio}</td>
+        <td><button class="btn btn-danger" onclick="eliminar('${doc.id}')">Eliminar</button></td>
+        <td><button class="btn btn-warning" onclick="editar('${doc.id}', '${doc.data().nombre}', '${doc.data().descripcion}', '${doc.data().precio}')">Editar</button></td>
+        </tr>
+        `
+
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+}
+
 
 }
 
@@ -51,7 +92,7 @@ tabla.innerHTML = ``;
         <td>${doc.data().descripcion}</td>
         <td>${doc.data().precio}</td>
         <td><button class="btn btn-danger" onclick="eliminar('${doc.id}')">Eliminar</button></td>
-        <td><button class="btn btn-warning" onclick="editar('${doc.id}', '${doc.data().nombre}', '${doc.data().descripcion}', '${doc.data().precio}')">Editar</button></td>
+        <td><button class="btn btn-warning" data-toggle="modal" data-target="#ModalNuevo" onclick="editar('${doc.id}', '${doc.data().nombre}', '${doc.data().descripcion}', '${doc.data().precio}')">Editar</button></td>
         </tr>
         `
     });
@@ -66,18 +107,18 @@ function eliminar(id){
     });
 }
 
-function editar(id, nombre, descripcion, precio){
+function editar(id1, nombre1, descripcion1, precio1){
 
-    document.getElementById("nombre").value = nombre;
-    document.getElementById("descripcion").value = descripcion;
-    document.getElementById("precio").value = precio;
-    
+    document.getElementById("nombre").value = nombre1;
+    document.getElementById("descripcion").value = descripcion1;
+    document.getElementById("precio").value = precio1;
+
     var boton = document.getElementById("boton");
     boton.innerHTML = 'Editar';
 
     boton.onclick = function(){
 
-        var medicamento = db.collection("medicamento").doc(id);
+        var medicamento = db.collection("medicamento").doc(id1);
 
         var name = document.getElementById("nombre").value;
         var description = document.getElementById("descripcion").value;
@@ -92,11 +133,10 @@ function editar(id, nombre, descripcion, precio){
         })
         .then(function() {
             console.log("Document successfully updated!");
-            boton.innerHTML = 'Guardar';
             document.getElementById("nombre").value = "";
             document.getElementById("descripcion").value = "";
-            document.getElementById("precio").value =0;
-
+            document.getElementById("precio").value = 0;
+            $('#ModalNuevo').modal('hide');
         })
         .catch(function(error) {
             // The document probably doesn't exist.
